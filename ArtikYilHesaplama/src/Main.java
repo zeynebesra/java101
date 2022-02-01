a4db5ff849ca24a107e195c81c559fe0420862d9
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-02-01 14:35:23 +0300

    artık yıl hesaplama

---
+++ ArtikYilHesaplama/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+/*
+Java ile kullanıcının girdiği yılın artık olup olmadığını bulan programı yazınız.
+Artık Yıl Nedir?
+Artık yıl, Miladî takvimde 365 yerine 366 günü olan yıl.
+Bu fazladan gün (artık gün), normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year;
+
+        System.out.print("Yıl Giriniz: ");
+        year = input.nextInt();
+
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    System.out.println(year + "Bir artık yıldır.");
+                }
+                else
+                {
+                    System.out.println(year + "Bir artık yıl değildir.");
+                }
+            }
+            else
+            {
+                System.out.println(year+ "Bir artık yıldır.");
+            }
+        }
+        else
+        {
+            System.out.println(year + "Bir artık yıl değildir.");
+        }
+
+    }
+}
